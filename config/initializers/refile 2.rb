d80fda73333e25ce625430c41c9b2b50db5ae157
shogo0625 <shogo19940625@gmail.com>
2020-05-11 12:33:13 +0000

    [Fix] aws_region表記変更

--- config/initializers/refile 2.rb
+++ config/initializers/refile 2.rb
@@ -2,7 +2,7 @@ require 'refile/s3'
 aws = {
   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  region: 'ap-northeast-1',
+  region: ENV['AWS_REGION'],
   bucket: ENV['AWS_S3_BUCKET_NAME']
 }
 Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
