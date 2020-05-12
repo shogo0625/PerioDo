6496c61a72b76873bc42b4b68a50edcd5593bab0
shogo0625 <shogo19940625@gmail.com>
2020-05-12 05:11:15 +0000

    [Change] refile.rb編集

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -7,3 +7,10 @@ aws = {
 }
 Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
 Refile.store = Refile::S3.new(prefix: 'store', **aws)
+
+s3_info = {
+	access_key_id: 'AWS_ACCESS_KEY_ID',
+	secret_access_key: 'AWS_SECRET_ACCESS_KEY',
+	region: 'AWS_REGION',
+	bucket: 'AWS_S3_BUCKET_NAME'
+}
