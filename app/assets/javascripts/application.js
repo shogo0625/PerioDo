cb001d6b8b9b22e506d358ad5df13ec0a0c329c4
shogo0625 <shogo19940625@gmail.com>
2020-05-13 10:19:20 +0000

    [Fix] ログアウト後もタイマー時間引継がれる不具合修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -132,6 +132,12 @@ $(function() {
     });
   });
 
+  $(function(){
+    $('#sign-out').click(function(){
+      timeToCountDown = 0;
+    });
+  });
+
   function redirectPage(){
     if (timeLeft > 1) {
       sessionStorage.setItem('number', timeLeft);
