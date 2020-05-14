e0294e515e3fd8ee6b73ef9eaa7021bce93c4397
shogo0625 <shogo19940625@gmail.com>
2020-05-14 09:17:42 +0000

    [Fix] TodoListの並び順変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,7 @@ class Task < ApplicationRecord
 
   enum status: { ToDo: 0, Doing: 1, Done: 2 }
 
+  default_scope->{order(time_limit: :asc)}
+
   validates :content, presence: true, length: { maximum: 50 }
 end
