ee7660750f9e82a35bab59c518e5248482963802
shogo0625 <shogo19940625@gmail.com>
2020-05-27 08:18:24 +0000

    [Change] cap設定変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.13.0"
+lock "~> 3.14.0"
 
 set :application, "PerioDo"
 set :repo_url, "git@github.com:shogo0625/PerioDo.git"
