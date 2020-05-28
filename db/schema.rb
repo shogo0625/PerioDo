eb126462d14f2b55fd754a5e94099fe52f63bf56
shogo0625 <shogo19940625@gmail.com>
2020-05-28 15:18:18 +0900

    [Change] 開発環境/vagrant ubuntu18.04 => docker, docker-compose

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_20_064442) do
 
-  create_table "favorites", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_favorites_on_post_id"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_064442) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
-  create_table "notifications", force: :cascade do |t|
+  create_table "notifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "visitor_id", null: false
     t.integer "visited_id", null: false
     t.integer "post_id"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_064442) do
     t.index ["visitor_id"], name: "index_notifications_on_visitor_id"
   end
 
-  create_table "post_comments", force: :cascade do |t|
+  create_table "post_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
     t.text "comment", null: false
@@ -45,14 +45,14 @@ ActiveRecord::Schema.define(version: 2020_05_20_064442) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "post_tags", force: :cascade do |t|
-    t.integer "post_id"
-    t.integer "tag_id"
+  create_table "post_tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "tag_id"
     t.index ["post_id"], name: "index_post_tags_on_post_id"
     t.index ["tag_id"], name: "index_post_tags_on_tag_id"
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.text "content", null: false
     t.string "image_id"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_064442) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "premade_tasks", force: :cascade do |t|
+  create_table "premade_tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.text "content", null: false
     t.time "time"
@@ -68,9 +68,9 @@ ActiveRecord::Schema.define(version: 2020_05_20_064442) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "follower_id"
+    t.bigint "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_064442) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-  create_table "routine_tasks", force: :cascade do |t|
+  create_table "routine_tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "routine_id"
     t.text "content", null: false
     t.time "time"
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_064442) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "routines", force: :cascade do |t|
+  create_table "routines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.string "title", null: false
     t.text "comment"
@@ -96,14 +96,14 @@ ActiveRecord::Schema.define(version: 2020_05_20_064442) do
     t.time "finish_time"
   end
 
-  create_table "tags", force: :cascade do |t|
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
-  create_table "tasks", force: :cascade do |t|
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.text "content", null: false
     t.integer "status", default: 0, null: false
@@ -112,7 +112,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_064442) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -129,4 +129,10 @@ ActiveRecord::Schema.define(version: 2020_05_20_064442) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "posts"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "post_tags", "posts"
+  add_foreign_key "post_tags", "tags"
+  add_foreign_key "relationships", "users", column: "followed_id"
+  add_foreign_key "relationships", "users", column: "follower_id"
 end
